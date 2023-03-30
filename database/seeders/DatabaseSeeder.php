2b000885f28604d655812cc2f849d5e6bc6b8408
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-03-30 19:16:34 +0200

    Created seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Train;
-
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,18 @@ class TrainSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $new_train = new Train;
+
+        $new_train->company = "Trenitalia";
+        $new_train->departure_station = "Roma Termini";
+        $new_train->arrival_station = "Roma Ostiense";
+        $new_train->departure_time = "10:40:40";
+        $new_train->arrival_time = "11:30:30";
+        $new_train->train_code = "3384";
+        $new_train->coach_number = "8";
+        $new_train->in_time = "1";
+        $new_train->cancelled = "0";
+
+        $new_train->save();
     }
 }
