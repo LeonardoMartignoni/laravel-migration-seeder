d07bd22520337bf1e6e28324f1eba95b0769a461
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-03-30 18:42:59 +0200

    Added cards

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -5,16 +5,41 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel 9 + Bootstrap Template</title>
+  <title>Trains</title>
 
   {{-- Includiamo gli assets con la direttiva @vite --}}
   @vite('resources/js/app.js')
 </head>
 
 <body>
-  @foreach ($trains as $train)
-    <p>{{ $train }}</p>
-  @endforeach
+  <div class="container">
+    <div class="row row-cols-4">
+      {{-- @foreach ($trains as $train)
+        <p>{{ $train }}</p>
+      @endforeach --}}
+      @foreach ($trains as $train)
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Train {{ $train['train_code'] }}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{ substr($train['departure_time'], 0, -3) }} ->
+                {{ substr($train['arrival_time'], 0, -3) }}
+              </h6>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">From: {{ ucwords($train['departure_station']) }}</li>
+              <li class="list-group-item">To: {{ ucwords($train['arrival_station']) }}</li>
+            </ul>
+            {{-- <div class="card-body">
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div> --}}
+          </div>
+        </div>
+      @endforeach
+    </div>
+  </div>
+
 </body>
 
 </html>
